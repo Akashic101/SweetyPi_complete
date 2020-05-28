7950aa347b4b2a45271b26bb07e436f70fbbdcc8
David Moll <david.leander.moll@gmail.com>
2020-05-28 15:41:11 +0200

    Fixed Twitch-message when someone resubs

--- index.js
+++ index.js
@@ -966,7 +966,6 @@ clientTWI.on("submysterygift", (channel, username, numbOfSubs, methods, userstat
 
 clientTWI.on("resub", (channel, username, months, message, userstate, methods) => {
     let cumulativeMonths = ~~userstate["msg-param-cumulative-months"];
-
     clientTWI.say('Redfur_13', "redfur4Love redfur4Love " + userstate.username + " just continued his subscription. He is now subscribed since " + cumulativeMonths + " months redfur4Love redfur4Love");
     clientDIS.channels.cache.get(chitchatChannel).send("<:Sweety_scared:713075786713661440> <:Sweety_scared:713075786713661440> " + username + " just continued his subscription. He is now subscribed since " + months + " months <:Sweety_scared:713075786713661440> <:Sweety_scared:713075786713661440>");
 });
