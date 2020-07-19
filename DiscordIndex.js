917481e2a1263167f82cc728c06dec8da2aa0990
David Moll <david.leander.moll@gmail.com>
2020-07-19 21:46:41 +0200

    Now checks for deleted messages

--- DiscordIndex.js
+++ DiscordIndex.js
@@ -116,6 +116,37 @@ client.on("messageReactionAdd", (reaction, user) => {
   }
 });
 
+client.on('messageDelete', async message => {
+
+	if (!message.guild) return;
+	const fetchedLogs = await message.guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MESSAGE_DELETE',
+  });
+  
+	const deletionLog = fetchedLogs.entries.first();
+
+	if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
+
+	const { executor, target } = deletionLog;
+
+  const messageDeletedEmbed = new Discord.MessageEmbed()
+  .setTitle('**Deleted message**')
+  .setColor("#c3032b")
+  .setTimestamp()
+  .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
+
+	if (target.id === message.author.id) {
+    messageDeletedEmbed.setDescription(`A message by **${message.author.tag}** was deleted by **${executor.tag}**.`)
+    messageDeletedEmbed.setThumbnail(message.author.displayAvatarURL({ format: 'jpg' }))
+	}	else {
+    messageDeletedEmbed.setDescription(`A message by **${message.author.tag}** was deleted, but I don't know by who`);
+    messageDeletedEmbed.setThumbnail(message.author.displayAvatarURL({ format: 'jpg' }))
+  }
+  client.channels.cache.get(process.env.SERVER_LOG_CHANNEL).send(messageDeletedEmbed);
+});
+
+
 client.on('message', async message => {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
