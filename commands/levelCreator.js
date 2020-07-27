6359ee8f10f07d98fd9547e6ee73cfb49be97ed0
David Moll <david.leander.moll@gmail.com>
2020-07-27 09:08:51 +0200

    Changed database-setup

--- commands/levelCreator.js
+++ commands/levelCreator.js
@@ -8,7 +8,7 @@ const levelSeq = new Sequelize('database', 'user', 'password', {
 	storage: 'level.sqlite',
 });
 
-const xp = levelSeq.define('xp', {
+const levelTable = levelSeq.define('levelTable', {
 	id: {
         primaryKey: true,
 	    type: Sequelize.INTEGER,
@@ -18,31 +18,24 @@ const xp = levelSeq.define('xp', {
         type: Sequelize.INTEGER,
         unique: true,
     },
-    minimum: {
+    xp_needed: {
 		type: Sequelize.INTEGER,
 		defaultValue: 0,
 		allowNull: false,
-    },
-    maximum: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
     }
 });
 
-xp.sync()
+levelTable.sync()
 
 module.exports = {
 	name: 'levelcreator',
 	description: 'creates levels',
 	async execute(client, message, args) {
 
-        const level = await xp.create({
+        const level = await levelTable.create({
             level: 0,
-            minimum: 0,
-            maximum: 5
+            xp_needed: 0
         });
-        console.log(`Level ${level.level}: from ${level.minimum} to ${level.maximum}`);
 
         var min = 0;
         var max = 5;
@@ -51,12 +44,11 @@ module.exports = {
             for (var i = 1; i <= 1000; i++) {
                 min = max + 1;
                 max = (max + 5 + (i * 2));
-                const level = await xp.create({
+                const level = await levelTable.create({
                     level: i,
-                    minimum: min,
-                    maximum: max
+                    xp_needed: min
                 });
-                console.log(`Level ${level.level}: from ${level.minimum} to ${level.maximum}`);
+                console.log(`Level ${levelTable.level} needs ${levelTable.xp_needed} XP`);
             }
         } catch (e) {
             console.log(e)
