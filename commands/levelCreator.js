e6ab2864a1de9d56ee33fe439348bbf55699edce
David Moll <david.leander.moll@gmail.com>
2020-07-27 20:27:12 +0200

    Added success-message

--- commands/levelCreator.js
+++ commands/levelCreator.js
@@ -50,6 +50,7 @@ module.exports = {
                 });
                 console.log(`Level ${levelTable.level} needs ${levelTable.xp_needed} XP`);
             }
+            message.channel.send(`All levels have been succesfully added`)
         } catch (e) {
             console.log(e)
         }
