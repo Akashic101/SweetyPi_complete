fd566a617348461ab7433b28900312faf723465d
David Moll <david.leander.moll@gmail.com>
2020-07-27 17:26:37 +0200

    Added Level

--- commands/level.js
+++ commands/level.js
@@ -68,41 +68,32 @@ module.exports = {
             .setTimestamp()
             .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
         const channel = message.client.channels.cache.get(process.env.SERVER_LOG_CHANNEL);
-        //channel.send(logEmbed);
+        channel.send(logEmbed);
 
         try {
-            //Find the user by searching through the database with the id
             const match = await level.findOne({where: {user_id: userID}});
 
-            //If a match was found
             if(match) {
-                const userLevel = await levelTable.findOne({
-                    where: {
-                        //Find the level by looking where match.xp is greater or equal than the xp_needed
-                        //EXAMPLE: User 1 has 10 xp. Level 1 goes from 0 to 5, Level 2 from 6 to 15. 
-                        xp_needed: {
-                            [Sequelize.gte]: match.xp
-                        }
-                    } 
+                const userLevel = await levelTable.findAll({
+                    attributes: ['xp_needed']
                 })
-
-                //If the level was found
+                const tagString = userLevel.map(t => t.xp_needed);
+                var index = tagString.findIndex(function(number) {
+                    return number > match.xp
+                })
                 if(userLevel) {
-
-                    //Do something with the result
                     const levelEmbed = new Discord.MessageEmbed()
-                    .setColor('#e7a09c')
-                    .setTitle(`**Level**`)
-                    .setDescription(`**${username}** is level ${userLevel.level + 1} and has ${match.xp} XP`)
-                    .setThumbnail(message.member.user.displayAvatarURL({ format: 'jpg' }))
-                    .setTimestamp()
-                    .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
-                message.channel.send(levelEmbed)
+                        .setColor('#e7a09c')
+                        .setTitle(`**Level**`)
+                        .setDescription(`**${username}** is level ${index} and has ${match.xp} XP`)
+                        .setThumbnail(message.member.user.displayAvatarURL({ format: 'jpg' }))
+                        .setTimestamp()
+                        .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
+                    message.channel.send(levelEmbed)
                 }
                 else {
-                    message.channel.send(`I cannot find that level ${username} for because I am stupid`)
-                }
-                
+                    message.channel.send(`I cannot find that level for ${username} (who has ${match.xp} XP btw), sorry about that`)
+                } 
             }
             else {
                 message.channel.send("I was unable to find that profile")
