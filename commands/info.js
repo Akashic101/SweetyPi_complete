33bf1a10622e404ec4d46f01be642c753b89f6dc
David Moll <david.leander.moll@gmail.com>
2020-09-30 13:38:12 +0200

    Removed log, added new info

--- commands/info.js
+++ commands/info.js
@@ -1,37 +1,32 @@
-var pjson = require('../package.json');
-const Discord = require('discord.js');
-
 module.exports = {
-	name: 'info',
-	description: 'Send info about the current Hot-Lap-Challenge!',
-	execute(client, message, args) {
+    name: 'info',
+    description: 'Sends info about the bot',
+    args: false,
+    modOnly: false,
+    color: '#66ffff',
+    execute(client, message, args) {
+        fs.readdir('./commands', (err, files) => {
+            let infoEmbed = new Discord.MessageEmbed()
+                .setTitle('info')
+                .setURL('https://github.com/Akashic101/SweetyPi')
+                .setColor('RANDOM')
+                .addFields({
+                    name: 'Creator',
+                    value: `<@320574128568401920>`,
+                    inline: true
+                }, {
+                    name: 'Version',
+                    value: pjson.version,
+                    inline: true
+                }, {
+                    name: 'Commands',
+                    value: files.length,
+                    inline: true
+                })
 
-        var date = new Date();
-
-        const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#f21d31')
-            .setTitle(`**help**`)
-            .addFields(
-                { name: 'Username', value: message.member.user.tag},
-                { name: 'Command', value: message.content},
-		    	{ name: 'Date', value: date}
-            )
-            .setThumbnail(message.member.user.displayAvatarURL({ format: 'jpg' }))
-            .setTimestamp()
-            .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
-        const channel = message.client.channels.cache.get(process.env.SERVER_LOG_CHANNEL);
-        channel.send(helpEmbed);
-
-        let infoEmbed = new Discord.MessageEmbed()
-            .setTitle('info')
-            .setURL('https://github.com/Akashic101/SweetyPi')
-            .setColor((Math.random()*0xFFFFFF<<0).toString(16))
-            .addFields(
-                { name: 'Creator', value: pjson.author, inline: true},
-                { name: 'Version', value: pjson.version, inline: true}
-            )
-            .setTimestamp()
-            .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png')
-        message.channel.send(infoEmbed);
-	},
+                .setTimestamp()
+                .setFooter(`${process.env.BOT_NAME} V ${pjson.version}`, process.env.BOT_PFP);
+            message.channel.send(infoEmbed);
+        })
+    },
 };
