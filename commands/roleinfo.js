e327b5812fb6a3c7ef9ac0c8ca0e2f568010bccf
David Moll <david.leander.moll@gmail.com>
2020-09-30 13:37:34 +0200

    Added roleinfo

---
+++ commands/roleinfo.js
@@ -0,0 +1,63 @@
+var pjson = require('../package.json');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'roleinfo',
+    description: 'Sends info about a specific role',
+    args: true,
+    args_length: 1,
+    channel: ['test-channel', '📱commands'],
+    modOnly: false,
+    color: '#2153f5',
+    execute(client, message, args) {
+        let role = message.mentions.roles.first();
+
+        let roleInfoEmbed = new Discord.MessageEmbed()
+            .setTitle('Role Info')
+            .setColor(role.color.toString(16))
+            .addFields({
+                name: 'Role name',
+                value: `<@&${role.id}>`,
+                inline: true
+            }, {
+                name: 'Role id',
+                value: role.id,
+                inline: true
+            }, {
+                name: 'Member count',
+                value: role.members.size,
+                inline: true
+            }, {
+                name: 'Mentionable',
+                value: changeFalseTrue(role.mentionable),
+                inline: true
+            }, {
+                name: 'Displayed separately',
+                value: changeFalseTrue(role.hoist),
+                inline: true
+            }, {
+                name: 'Displayed separately',
+                value: changeFalseTrue(role.hoist),
+                inline: true
+            }, {
+                name: 'Managed by other extension',
+                value: changeFalseTrue(role.managed),
+                inline: true
+            }, {
+                name: 'Color',
+                value: `#${role.color.toString(16)}`,
+                inline: true
+            })
+            .setTimestamp()
+            .setFooter('SweetyPi V' + pjson.version, 'https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png');
+        message.channel.send(roleInfoEmbed);
+    },
+};
+
+function changeFalseTrue(value) {
+    if (value == true) {
+        return "Yes";
+    } else {
+        return "No";
+    }
+}
