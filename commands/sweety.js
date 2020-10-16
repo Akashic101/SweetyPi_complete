6a884eabf9b43f501422b2ab76933d3b85f4bd5c
David Moll <david.leander.moll@gmail.com>
2020-10-16 22:22:46 +0200

    Removed log, fixed description

--- commands/sweety.js
+++ commands/sweety.js
@@ -32,24 +32,8 @@ const SweetyImages = sweetyImagesSeq.define(`sweetyImages`, {
 
 module.exports = {
 	name: `sweety`,
-	description: `Send info about the current Hot-Lap-Challenge!`,
+	description: `Sends a random image/video of Sweety`,
 	async execute(client, message, args) {
-
-		var date = new Date();
-
-		const sweetyEmbed = new Discord.MessageEmbed()
-			.setColor(`#6f77e9`)
-			.setTitle(`**Sweety**`)
-			.addFields(
-				{ name: `Username`, value: message.member.user.tag},
-				{ name: `Command`, value: message.content},
-				{ name: `Date`, value: date}
-			)
-			.setThumbnail(message.member.user.displayAvatarURL({ format: `jpg` }))
-			.setTimestamp()
-			.setFooter(`SweetyPi V` + pjson.version, `https://cdn.discordapp.com/app-icons/683749467304099888/1127276baab40eb23bb680a8a102356b.png`);
-		const channel = message.client.channels.cache.get(process.env.SERVER_LOG_CHANNEL);
-		channel.send(sweetyEmbed);
 
 		try {
 			const match = await SweetyImages.findOne({ order: Sequelize.literal(`random()`) });
