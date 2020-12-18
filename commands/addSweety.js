0f238b82476a0cf3c662453b3c18637af95f2a93
David Moll <david.leander.moll@gmail.com>
2020-12-18 14:03:26 +0100

    Added functionality to add images from imgur-album

--- commands/addSweety.js
+++ commands/addSweety.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 const Sequelize = require(`sequelize`);
+const imgur = require(`imgur`);
 
 const sweetyImagesSeq = new Sequelize(`database`, `user`, `password`, {
 	host: `localhost`,
@@ -27,27 +28,33 @@ const SweetyImages = sweetyImagesSeq.define(`sweetyImages`, {
 });
 
 module.exports = {
-	name: `addSweety`,
+	name: `addsweety`,
 	modOnly: true,
 	args: true,
 	args_length: 1,
-	description: `Adds a image/video to the sweety-database`,
+	description: `Adds images from an Imgur-Album to the Sweety-database`,
 	color: `#162fb8`,
 	async execute(client, message, args) {
 
-		try {
-			for(var i = 0; i < args.length; i++) {
-				const add = await SweetyImages.create({
-					link: args[i]
-				});
-				message.channel.send(`Link ${add.link} added.`);
-			}
-		} catch (e) {
-			if (e.link === `SequelizeUniqueConstraintError`) {
-				return message.channel.send(`That link already exists.`);
-			}
-			return message.channel.send(`Something went wrong with adding a link.`);
-		}
-		
+		var album = args[0];
+
+		console.log(args[0]);
+		imgur.getAlbumInfo(album)
+			.then(async function (json) {
+				try {
+					for (var i = 0; i < json.data.images_count; i++) {
+
+						await SweetyImages.create({
+							link: json.data.images[i].link
+						});
+					}
+					message.channel.send(`All ${json.data.images_count} images and videos have been added`);
+				} catch (e) {
+					if (e.link === `SequelizeUniqueConstraintError`) {
+						return message.channel.send(`That link already exists.`);
+					}
+					return message.channel.send(`Something went wrong with adding a link.`);
+				}
+			});
 	},
 };
